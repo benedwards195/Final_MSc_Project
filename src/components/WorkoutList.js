91e51c6ea3175333eb9b6cf259d5c18b3c7b9b8a
Benjamin Edwards <edwardsbt@cardiff.ac.uk>
2023-01-03 17:55:59 +0000

    Workout Instructions Update

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -5,18 +5,18 @@ export default function WorkoutList() {
 <div className="WorkoutList">
     <h1>Bench Press Instructions</h1>
     <ol>
-    <li>1. Hold the weight directly over the shoulders, arms slightly wider than shoulder height.</li> 
-    <li>2.Push the weight up until your arms are angled at 45 degrees at the top.</li> 
-    <li>3. Slowly lower the weight back down to chest height, elbows out to the sides.</li> 
+    <li>Hold the weight directly over the shoulders, arms slightly wider than shoulder height.</li> 
+    <li>Push the weight up until your arms are angled at 45 degrees at the top.</li> 
+    <li>Slowly lower the weight back down to chest height, elbows out to the sides.</li> 
     
     </ol>
     <h1>Dumbbell Chest Flies</h1>
     <ol>
-   <li>1. Start with your back flat on an incline bench, which is lowered to 30 degrees. Hold 1 dumbbell in each hand.</li>
-   <li>2. Start with your arms at chest level at your sides, elbows bent and pointing out.</li>
-   <li>3. Slowly exhale and lift your arms above your chest.</li>
-   <li>4. Inhale and slowly lower your arms to your sides to the starting position.</li>
-   <li>5. Continue to press up.</li>
+   <li>Start with your back flat on an incline bench, which is lowered to 30 degrees. Hold 1 dumbbell in each hand.</li>
+   <li>Start with your arms at chest level at your sides, elbows bent and pointing out.</li>
+   <li>Slowly exhale and lift your arms above your chest.</li>
+   <li>Inhale and slowly lower your arms to your sides to the starting position.</li>
+   <li>Continue to press up.</li>
    <li> https://www.healthline.com/health/exercise-fitness/bench-press-muscles-worked#:~:text=Hold%20the%20weight%20directly%20over,around%205%20reps%20or%20more.</li>
     </ol>
     <h1>Deadlifts</h1>
