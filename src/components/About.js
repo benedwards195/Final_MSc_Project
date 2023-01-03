1df88a194fce9f2e0fb2a662bd977db73e0017a7
Benjamin Edwards <edwardsbt@cardiff.ac.uk>
2023-01-03 13:36:39 +0000

    Update to about page

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
         <>
     <h1>About Me</h1>
     <h2>Founded in 2022 by Mr BT Edwards</h2>
-    <h3>Address: 120 Lapland, Cardiff, CF24 0BE</h3>
+    <h3>Address: 120 Cardiff Road, Cardiff, CF24 0BE</h3>
     <h4>Email address: EdwardsBT@user.com</h4>
     <h4>Phone Number: 07123456789</h4>
 
