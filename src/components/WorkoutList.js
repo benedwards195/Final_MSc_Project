aad58892db687dd68095fc54aaed7beb547466c8
Benjamin Edwards <edwardsbt@cardiff.ac.uk>
2023-01-05 00:57:39 +0000

    Updates to prototype

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -5,20 +5,23 @@ export default function WorkoutList() {
 <div className="WorkoutList">
     <h1>Bench Press Instructions</h1>
     <ol>
-    <li>Hold the weight directly over the shoulders, arms slightly wider than shoulder height.</li> 
+    <li>Hold the weight directly over the shoulders, positioning your arms slightly wider than shoulder height, but parallel.</li> 
     <li>Push the weight up until your arms are angled at 45 degrees at the top.</li> 
-    <li>Slowly lower the weight back down to chest height, elbows out to the sides.</li> 
-    
+    <li>Then gradually lower the weight back down to chest height, elbows out to the sides.</li>    
     </ol>
+    <p>Credit:  Chertoff, J(2022) https://www.healthline.com/health/exercise-fitness/bench-press-muscles-worked#:~:text=Hold%20the%20weight%20directly%20over,around%205%20reps%20or%20more.</p> 
+
     <h1>Dumbbell Chest Flies</h1>
     <ol>
-   <li>Start with your back flat on an incline bench, which is lowered to 30 degrees. Hold 1 dumbbell in each hand.</li>
+   <li>Pick two matching dumbbell weights, then find a flat bench.</li>
+   <li>Start with your back flat on the bench. Hold 1 dumbbell in each hand.</li>
    <li>Start with your arms at chest level at your sides, elbows bent and pointing out.</li>
    <li>Slowly exhale and lift your arms above your chest.</li>
    <li>Inhale and slowly lower your arms to your sides to the starting position.</li>
    <li>Continue to press up.</li>
-   <li> https://www.healthline.com/health/exercise-fitness/bench-press-muscles-worked#:~:text=Hold%20the%20weight%20directly%20over,around%205%20reps%20or%20more.</li>
     </ol>
+    <p> Credit: Chertoff, J(2022) https://www.healthline.com/health/exercise-fitness/bench-press-muscles-worked#:~:text=Hold%20the%20weight%20directly%20over,around%205%20reps%20or%20more.</p>
+
     <h1>Deadlifts</h1>
     <ol>
         <li>Stand with your mid-foot under the barbell</li>
@@ -26,15 +29,20 @@ export default function WorkoutList() {
         <li>Bend your knees until your shins touch the bar</li>
         <li>Lift your chest up and straighten your lower back</li>
         <li>Take a big breath, hold it, and stand up with the weight</li>
-        <li>- https://stronglifts.com/deadlift/</li>
+        <li>As you pull the weight up, hold it for a second at the top, with locked hips and knees. Then return the weight to the floor, making sure you move your hips back while bending your legs. Rest for one second at the bottom, then repeat.</li>
+        <li>You must ensure that your lower back stays neutral in order to avoid injury. If you round it it during heavy Deadlifts, then that is dangerous for your spine. It puts uneven pressure on your spinal discs which can injure them. Always Deadlift with a neutral lower back – maintain the natural inward curve of your lower spine.</li>
+        <li>Please start off with a weight that is light for you, until you have mastered your form. The consequences of picking up a severe injury is greater if your form for deadlifts isn't correct.</li>
+        
     </ol>
+    <p>Credit: https://stronglifts.com/deadlift/</p>
        <h1>Bent Over Row</h1>
        <ol>
            <li>Grab a set of dumbbells, bend your torso forward and keep your knees slightly bent.</li>
            <li>Pull the dumbbells toward your waistline, while squeezing your shoulder blades.</li>
            <li>Slowly lower the weights to the starting position. A 1-second pull, 1-second pause, 2 second down count is ideal.</li>
-           <li>https://www.spotebi.com/exercise-guide/dumbbell-bent-over-row/</li>
+           
        </ol>
+       <p>Credit: https://www.spotebi.com/exercise-guide/dumbbell-bent-over-row/</p>
        <h1>Assisted Pull Ups</h1>
        <ol>
            <li>Loop a resistance band around the bar to create a hanging loop.</li>
@@ -42,32 +50,35 @@ export default function WorkoutList() {
            <li>Place either your knees or feet in the band.</li>
            <li>Raise your body as high as you can.</li>
            <li>Slowly lower back down to the starting position.</li>
-           <li>https://www.healthline.com/health/assisted-pull-ups#resistance-band-pullups</li>
        </ol>
+       <p>Credit: Cronkleton, E (2020) https://www.healthline.com/health/assisted-pull-ups#resistance-band-pullups</p>
+
       <h1>Lat Pulldown</h1>
       <ol>
           <li>Grasp the bar with a wide grip with an overhand, knuckles-up grip. Other positions and grips are possible but start with this standard position.</li>
           <li>Pull the bar down until it's approximately level with the chin. Exhale on the downward motion. While shifting slightly backward is OK, aim to keep your upper torso stationary. Keep your feet flat on the floor and engage your abs as you pull. The bottom of the motion should be where your elbows can't move downward anymore without moving backward. Be sure to stop at that point and do not go lower.</li>
           <li>Squeeze the shoulder blades together while maintaining square shoulders.</li>
           <li>From the bottom position, with the bar close to your chin, slowly return the bar to the starting position while controlling its gradual ascent. Don't let it crash into the weight plates.</li>
-          <li>https://www.verywellfit.com/how-to-do-the-lat-pulldown-3498309</li>
-          <li>Continue until you complete eight to 12 repetitions in a set. Rest, then continue to complete your program of sets.</li>
       </ol>
+      <p>Credit: Black, H (2022) https://www.verywellfit.com/how-to-do-the-lat-pulldown-3498309</p>
+
        <h1>Standing Shoulder Dumbbell Press</h1>
        <ol>
            <li>Stand upright and keep the back straight. Hold a dumbbell in each hand, at the shoulders, with an overhand grip. Thumbs are on the inside and knuckles face up.</li>
            <li>Exhale as you raise the weights above the head in a controlled motion.</li>
            <li>Pause briefly at the top of the motion.</li>
            <li>Inhale and return the dumbbells to the shoulders.</li>
-           <li>https://www.verywellfit.com/how-to-do-the-dumbbell-overhead-press-3498298</li>
+           
        </ol>
+       <p>Credit: Black, H (2022) https://www.verywellfit.com/how-to-do-the-dumbbell-overhead-press-3498298</p>
        <h1>Squats</h1>
        <ol>
            <li>Stand up with your feet shoulder-width apart.</li>
            <li>Bend your knees, press your hips back and stop the movement once the hip joint is slightly lower than the knees</li>
            <li>Press your heels into the floor to return to the initial position.</li>
-           <li>https://www.spotebi.com/exercise-guide/squat/</li>
+           
        </ol>
+       <p>Credit: https://www.verywellfit.com/how-to-do-the-dumbbell-overhead-press-3498298</p>
        <h1>Leg Press</h1>
        <ol>
            <li>When you sit down at a leg press machine, your body should be in a particular position. Sit on the machine with your back and head resting comfortably against the padded support. Place your feet on the footplate about hip-width apart while ensuring that your heels are flat.</li>
@@ -77,8 +88,10 @@ export default function WorkoutList() {
            <li>While exhaling, extend your legs and keep your head and back flat against the seat pad. Extend with slow control rather than with an explosive movement.</li>
            <li>Pause at the top of the movement. Do not lock out your knees, and ensure that they are not bowing out or in.</li>
            <li>While inhaling, return the footplate to the starting position by gradually bending the knees. Keep the feet and back flat throughout.</li>
-           <li>If you have never done leg presses, start modestly with three sets of 10 leg presses. You can advance from there as you build strength.</li>
+           
        </ol>
+       <p>Credit: Rogers, P (2022) https://www.verywellfit.com/how-to-do-the-leg-press-3498610</p>
+
 </div>
     )
 }
